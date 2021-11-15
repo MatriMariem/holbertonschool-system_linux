d1e8907fa2edeb2d74437700f65b692169bc8500
MatriMariem <meriemmatri1994@gmail.com>
2021-11-15 21:26:27 +0100

    test checker

--- 0x00-ls/ls.c
+++ 0x00-ls/ls.c
@@ -1,27 +1,80 @@
-#include "ls.h"
+#include "hls.h"
 
 /**
- * main - create my own ls
- * Return: 0 on success
+ * hls - lists directory contents
+ * @argc: number of arguments
+ * @argv: arguments (directory and/or options)
+ * Return: 0 on success, 2 on failure
  */
-int main(void)
+unsigned int hls(int argc, char *argv[])
 {
 	DIR *dir;
-	struct dirent *read;
+	struct dirent *dent;
+	char *directory = ".";
+	int i, space, new_line;
+	unsigned int exit_status = 0;
 
-	dir = opendir(".");
-	if (dir == NULL)
+	for (i = 1, space = 0; i < argc || argc == 1; i++, space = 0)
 	{
-		printf("Error, unable to open the directory!");
-		exit(1);
+		if (argc > 1)
+			directory = argv[i];
+		dir = opendir(directory);
+		if (dir == NULL)
+		{
+			exit_status = error_handler(directory);
+			continue;
+		}
+		if (argc > 2)
+			printf("%s:\n", argv[i]);
+		while ((dent = readdir(dir)) != NULL)
+		{
+			if (*dent->d_name == '.')
+				continue;
+			if (space == 1)
+				printf(" %s", dent->d_name);
+			else
+				printf("%s", dent->d_name);
+			space = 1;
+			new_line = 1;
+		}
+		if (new_line == 1)
+			printf("\n");
+		if (argv[i + 1] != NULL && opendir(argv[i + 1]) != NULL)
+			printf("\n");
+		closedir(dir);
+		if (argc == 1)
+			break;
 	}
+	return (exit_status);
+}
 
-		while ((read = readdir(dir)) != NULL)
-			printf("%s\t", read->d_name);
+/**
+ * error_handler - handles errors for hls
+ * @directory: directory to search in
+ * Return: 2 for failure
+ */
+unsigned int error_handler(char *directory)
+{
+	char buf[BUFSIZ];
 
-	printf("\n");
+	if (errno == ENOENT)
+		sprintf(buf, "hls: cannot access %s", directory);
+	else if (errno == EACCES)
+		sprintf(buf, "hls: cannot open directory %s", directory);
+	perror(buf);
+	return (2);
+}
 
-	closedir(dir);
+/**
+ * main - entry point for program
+ * @argc: number of arguments
+ * @argv: arguments given
+ * Return: 0 on success, 2 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int exit_status;
 
-	return (0);
+	exit_status = hls(argc, argv);
+	return (exit_status);
 }
--- 0x00-ls/ls.h
+++ 0x00-ls/ls.h
@@ -1,10 +1,21 @@
-#ifndef LS_H
-#define LS_H
+#ifndef HLS_H
+#define HLS_H
 
-#include <sys/types.h>
 #include <dirent.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+unsigned int error_handler(char *directory);
+unsigned int hls(int argc, char *argv[]);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
-#endif
+#endif /*HLS_H*/
